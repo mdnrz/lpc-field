bfa6233863be21c2f53fdd3743844b7a06449980
mdnrz <mehdeenoroozi@gmail.com>
2025-07-23 10:35:14 +0330

    fix version hash extraction; check for tags

--- src/globals/make-version.mk
+++ src/globals/make-version.mk
@@ -1,11 +1,16 @@
 GLOBAL_DIR = $(SRC_DIR)/globals
 ###############################################################################
 # Versioning
+HAS_TAG := $(shell git tag -l "v[0-9]*.[0-9]*")
+ifeq ($(HAS_TAG),)
+$(info No tags found. Define a tag like 'v0.1' to get versioning in build outputs)
+else
 VERSION = $(shell git describe --always --abbrev=4 --long)
 VERSION_MAJOR = $(shell git describe --always --long | sed "s/^v\([0-9]\+\).*/\1/")
 VERSION_MINOR = $(shell git describe --always --long | sed "s/^v[0-9]\+\.\([0-9]\+\).*/\1/")
 VERSION_PATCH = $(shell git describe --always --long | sed "s/^v[0-9]\+\.[0-9]\+-\([0-9]\+\).*/\1/")
-VERSION_HASH  = $(shell git describe --always --abbrev=4 --long | sed "s/^v[0-9]\+\.[0-9]\+-[0-9]\+-\([a-Z0-9]\{5\}\).*/\1/")
+VERSION_HASH  = $(shell git describe --always --abbrev=4 --long | sed "s/^v[0-9]\+\.[0-9]\+-[0-9]\+-\([a-z0-9]\{5\}\).*/\1/")
+endif
 
 VERSION_FILE = $(GLOBAL_DIR)/version.h
 
